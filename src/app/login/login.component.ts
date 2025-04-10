28c77586c5aab59604168a3cbbb2058bd1231539
0xM4rc <marcos.sanchez2003@proton.me>
2025-04-10 16:48:44 +0100

    Añadido pagina de login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  imports: [IonicModule], // <== IMPORTANTE,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
