a905c6d871091ae68838b42f315180fc201797cc
Carlos <dominguezdopazocarlos@gmail.com>
2025-04-10 18:15:13 +0100

    Bug fixes, project re-structuring

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,10 +7,14 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
+
+  goBack() {
+    window.history.back();
+  }
 
 }
